f1c89c8441c0d44f71ddf6dfaeb905d8b6f5768d
jayrcastro29 <castroarmandojr29@gmail.com>
2018-06-14 14:19:38 +0800

    crud/admin|user

---
+++ app/models/movie.rb
@@ -0,0 +1,9 @@
+class Movie < ApplicationRecord
+# def self.search(search)
+#   if search
+#     find(:all, :conditions => ['title LIKE ?', "%#{search}%"])
+#   else
+#     find(:all)
+#   end
+# end
+end
